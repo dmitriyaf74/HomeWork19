3b8375946c44b44558b4ae6f2baf4be39c552110
Dmitriy Filippov <dmitriy.filippov.reid@gmail.com>
2025-01-11 11:04:11 +0500

    Fix Program2

--- Classes/CatalogOfPlanet.cs
+++ Classes/CatalogOfPlanet.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HomeWork19.Classes
 {
-    internal class CatalogOfPlanet : List<Planeta>
+    internal class CatalogOfPlanet
     {
         int queryCounter = 0;
+        private List<Planeta> PlanetList;
+
+        public CatalogOfPlanet() 
+        {
+            PlanetList = new List<Planeta>();
+            Planeta planeta = new Planeta() { Name = "Венера", Numerus = 2, Aequator = 38025, Prior = null };
+            PlanetList.Add(planeta);
+            planeta = new Planeta() { Name = "Земля", Numerus = 3, Aequator = 40075, Prior = planeta };
+            PlanetList.Add(planeta);
+            planeta = new Planeta() { Name = "Марс", Numerus = 4, Aequator = 21344, Prior = planeta };
+            PlanetList.Add(planeta);
+        }
+
         public (int numerus, int aequator, string errstr) GetPlanet(string planetname) 
         {
             queryCounter++;
@@ -16,7 +31,7 @@ namespace HomeWork19.Classes
             {
                 return (0, 0, "Вы спрашиваете слишком часто");
             }
-            foreach (var pl in this)
+            foreach (var pl in this.PlanetList)
             {
                 if (pl.Name == planetname)
                 {
--- Program2.cs
+++ Program2.cs
@@ -18,7 +18,7 @@ namespace HomeWork19
             string errstr;
             (numerus, aequator, errstr) = catalogOfPlanet.GetPlanet(planetName);
             Console.WriteLine(planetName);
-            if (aequator == 0)
+            if (!string.IsNullOrEmpty(errstr))
             {
                 Console.WriteLine(errstr);
             }
@@ -32,20 +32,11 @@ namespace HomeWork19
 
         public static void Execute()
         {
-            var catalogOfPlanet = new CatalogOfPlanet()
-            {
-                new Planeta(){ Name = "Венера", Numerus = 2, Aequator = 38025, Prior = null },
-                new Planeta(){ Name = "Земля" , Numerus = 3, Aequator = 40075, Prior = null },
-                new Planeta(){ Name = "Марс"  , Numerus = 4, Aequator = 21344, Prior = null },
-            };
-            for (int i = 0; i < catalogOfPlanet.Count - 1; i++)
-            {
-                catalogOfPlanet[i + 1].Prior = catalogOfPlanet[i];
-            }
+            var catalogOfPlanet = new CatalogOfPlanet();
 
-            ShowPlanetInfo(catalogOfPlanet, "Земля");
+            ShowPlanetInfo(catalogOfPlanet, "Земля"  );
             ShowPlanetInfo(catalogOfPlanet, "Лимония");
-            ShowPlanetInfo(catalogOfPlanet, "Марс");
+            ShowPlanetInfo(catalogOfPlanet, "Марс"   );
 
         }
 
