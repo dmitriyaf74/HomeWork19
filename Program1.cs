153551ba9631d4fa0c6c9dfd56a4ff3d6f808cd0
Dmitriy Filippov <dmitriy.filippov.reid@gmail.com>
2025-01-11 10:38:11 +0500

    Fix Program1

--- Program1.cs
+++ Program1.cs
@@ -2,13 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using HomeWork19.Classes;
 
 namespace HomeWork19
 {
     static class Program1
     {
+        private static void ShowPlanet(string planetname, int planetnum, int aequator, bool venusEqv)
+        {
+            Console.WriteLine(planetname);
+            Console.WriteLine($"Порядковый номер от солнца: {planetnum}");
+            Console.WriteLine($"Длина экватора: {aequator} км");
+            Console.WriteLine($"Эквивалент Венеры: {venusEqv}");
+            Console.WriteLine("");
+        }
 
         public static void Execute()
         {
@@ -33,24 +44,17 @@ namespace HomeWork19
                 Aequator = 21344,
                 Prior = (Object)Terra,
             };
-            var Venus2 = new
-            {
-                Name = "Венера",
-                Numerus = 2,
-                Aequator = 38025,
-                Prior = (Object)Mars,
-            };
 
-            dynamic planeta = Venus2;
-            while (planeta != null)
-            {
-                Console.WriteLine(planeta.Name);
-                Console.WriteLine($"Порядковый номер от солнца: {planeta.Numerus}");
-                Console.WriteLine($"Длина экватора: {planeta.Aequator} км");
-                Console.WriteLine($"Эквивалент Венеры: {planeta.Equals(Venus)}");
-                Console.WriteLine("");
-                planeta = planeta.Prior;
-            }
+            var Venus2 = Venus with {};
+
+            dynamic planeta = Venus;
+            ShowPlanet(planeta.Name, planeta.Numerus, planeta.Aequator, planeta.Equals(Venus));
+            planeta = Terra;
+            ShowPlanet(planeta.Name, planeta.Numerus, planeta.Aequator, planeta.Equals(Venus));
+            planeta = Mars;
+            ShowPlanet(planeta.Name, planeta.Numerus, planeta.Aequator, planeta.Equals(Venus));
+            planeta = Venus2;
+            ShowPlanet(planeta.Name, planeta.Numerus, planeta.Aequator, planeta.Equals(Venus));
         }
     }
 }
